8b30a84b5b174d3e9b7cded70b185bae1f52528c
sourabha s mohanty <sourabhasikun20@gmail.com>
2023-08-21 10:05:59 +0530

    Blog-App

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name') }}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- //Include bootstrap CSS CDN here -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+        <style>
+            body {
+                font-family: 'Nunito';
+            }
+        </style>
+
+    </head>
+
+    <body>
+    @yield('content')
+
+    </body>
+    <!-- //Include bootstrap JS CDN here / -->
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/blog', [\App\Http\Controllers\BlogPostController::class, 'index']);
+Route::get('/blog/{blogPost}',[\App\Http\Controllers\BlogPostController::class,'show']);
+Route::get('/blog/create/post', [\App\Http\Controllers\BlogPostController::class, 'create']);
+Route::post('/blog/create/post', [\App\Http\Controllers\BlogPostController::class, 'store']);
+Route::get('/blog/{blogPost}/edit', [\App\Http\Controllers\BlogPostController::class, 'edit']); //shows edit post form
+Route::put('/blog/{blogPost}/edit', [\App\Http\Controllers\BlogPostController::class, 'update']); //commits edited post to the database 
+Route::delete('/blog/{blogPost}', [\App\Http\Controllers\BlogPostController::class, 'destroy']); //deletes post from the database
